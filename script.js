a9da1b6f8528b89dfda53acbc7b834274e0fc2a9
JSScot <153024062+JSScot@users.noreply.github.com>
2024-02-14 21:59:48 +0000

    figured out how game paths will work

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 console.log("sff")
 //for changing background easier
-let x = -1;
+let x = 0;
 //determines what is the background, choices, etc based on what you chose
-let path = 0;
+let path = -1;
 
+let opt1=""
+let opt2=""
+
+  
+  
 //changes background based on path
 function bg()
 {
-  x++;
   const folder = "backG/"
-let scene = ["house.jpg","nn"];
+let scene = ["house.jpg","road.jpg"];
 
- document.body.style.backgroundImage = "url("+folder+ scene[x]+")";
+ document.body.style.backgroundImage = "url("+folder+ scene[path]+")";
 }
+
+
 
 let text = document.getElementById("main_text")
 document.getElementById("start").addEventListener("click", startGame )
@@ -21,46 +27,76 @@ let left = document.getElementById("left");
 let right = document.getElementById("right");
 let images = document.getElementById("images");
 
-right.addEventListener("click", pathChanger)
-left.addEventListener("click",pathChanger)
+//when choices are clicked
+left.addEventListener("click", () => pathChanger("left"))
+right.addEventListener("click",() => pathChanger("right"))
 
-
+//the start of the game
 function startGame()
 {
-  bg();
+  path++;
+  game()
   document.getElementById("start").style.visibility = "hidden";
   left.style.visibility = "visible";
   right.style.visibility = "visible";
   images.style.visibility = "visible";
-  text.innerHTML = "You start at home, what's your first move?";
  
 }
 
+//image box changes when hovering over button choices 
 function changePix(choice)
 {
+  
   if(choice == "left")
   {
-    images.src = "images/car.jpg"
+    images.src = "images/"+opt1+".jpg"
   }
   else if(choice == "right")
   {
-     images.src = "images/tv.jpg"
+     images.src = "images/"+opt2+".jpg"
   }
 }
 
 function game()
 {
-  switch(x){
+  bg()
+  switch(path){
     case 0:
-      text.innerHTML = "it worked";
-      left.innerHTML = "bla bla bla"
-        right.innerHTML = "dododo:"
-      bg();
+      text.innerHTML = "You start at home, what's your first move?";
+      left.innerHTML = "Get in your car and drive"
+        right.innerHTML = "Watch Tv for a bit"
+      opt1="car";
+      opt2="tv";
       break;
+    case 1:
+      text.innerHTML = "You hit the rode. You got in your car and were able to drive 10 miles before your car ran out of gas. What do you do now?";
+        left.innerHTML = "call uber"
+        right.innerHTML = "Call a close friend"
+      opt1="uber";
+      opt2="friend";
+      break;
+      
     default:
-      text.innerHTML = "it didn't work";
+      text.innerHTML = "N/A";
+      opt1="";
+      opt2="";
       break;
 
   }
+}
+
+function pathChanger(choice)
+{
+  if(choice == "left")
+  {
+    path++;
+    game();
+  }
+  else
+  {
+    path+=4;
+    game();
+  }
+ console.log(path);
 }
 
